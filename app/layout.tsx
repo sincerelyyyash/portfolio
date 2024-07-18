29a9fb7b133f53f2de4df00dc88a47f7b3d927db
sincerelyyyash <yta0088@gmail.com>
2024-07-18 23:21:55 +0530

    Tracing beam component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Palanquin } from "next/font/google";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
 
 const palanquin = Palanquin({
@@ -22,12 +23,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={palanquin.className}>
-        <div className="relative w-full flex items-center justify-center flex-col">
-          <Navbar />
-          <div className="mt-10 w-full">
-            {children}
+        <TracingBeam className="px-4">
+          <div className="relative w-full flex items-center justify-center flex-col">
+            <Navbar />
+            <div className="mt-10 w-full">
+              {children}
+            </div>
           </div>
-        </div>
+        </TracingBeam>
       </body>
     </html>
   );
