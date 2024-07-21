e4477b33ac5112b83937fb0eff711239cfcc8720
sincerelyyyash <yta0088@gmail.com>
2024-07-21 23:45:51 +0530

    Skills data added

---
+++ utils/skillsData.ts
@@ -0,0 +1,44 @@
+
+const skills = {
+  default: [
+    { skill: "NextJS", img: "/path" },
+    { skill: "React", img: "/path/to/postgresqlimg.png" },
+    { skill: "PostgreSQL", img: "/path/to/postgresqlimg.png" },
+    { skill: "MongoDB", img: "/path/to/postgresqlimg.png" },
+    { skill: "NodeJS", img: "/path/to/postgresqlimg.png" },
+    { skill: "Typescript", img: "/path/to/postgresqlimg.png" },
+    { skill: "Javascript", img: "/path/to/postgresqlimg.png" },
+    { skill: "Turborepo", img: "/path/to/postgresqlimg.png" },
+    { skill: "Github", img: "/path/to/postgresqlimg.png" },
+    { skill: "Docker", img: "/path/to/postgresqlimg.png" },
+    { skill: "AWS", img: "/path/to/postgresqlimg.png" },
+
+  ],
+  frontend: [
+    { skill: "React", img: "/path/to/reactimg.png" },
+    { skill: "NextJS", img: "/path" },
+    { skill: "Flutter", img: "/path/to/vueimg.png" },
+  ],
+  backend: [
+    { skill: "Node.js", img: "/path/to/nodeimg.png" },
+    { skill: "Express.js", img: "/path/to/expressimg.png" },
+    { skill: "Firebase", img: "/path/to/postgresqlimg.png" },
+  ],
+  database: [
+    { skill: "MongoDB", img: "/path/to/mongodbimg.png" },
+    { skill: "PostgreSQL", img: "/path/to/postgresqlimg.png" },
+    { skill: "MySQL", img: "/path/to/mysqlimg.png" },
+    { skill: "Firebase", img: "/path/to/postgresqlimg.png" },
+  ],
+  tools: [
+    { skill: "Docker", img: "/path/to/dockerimg.png" },
+    { skill: "Git", img: "/path/to/gitimg.png" },
+    { skill: "Github", img: "/path/to/postgresqlimg.png" },
+    { skill: "Vscode", img: "/path/to/postgresqlimg.png" },
+    { skill: "Neovim", img: "/path/to/postgresqlimg.png" },
+  ],
+  devops: [
+    { skill: "AWS", img: "/path/to/awsimg.png" },
+    { skill: "CI/CD", img: "/path/to/cicdimg.png" },
+  ],
+};
