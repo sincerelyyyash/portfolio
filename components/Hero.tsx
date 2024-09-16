ea18d285da898c532547c44f0700c0f7c90b26f0
sincerelyyyash <yta0088@gmail.com>
2024-09-17 01:55:39 +0530

    Hero complete

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,48 +1,43 @@
-"use client"
+"use client";
 import Image from 'next/image';
-import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <motion.div
-      initial={{ opacity: 0.5, y: 100 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.1,
-        duration: 0.5,
-        ease: 'easeInOut',
-      }}
-      className="flex flex-wrap justify-center"
-    >
-      <section className="flex flex-col md:flex-row justify-start w-full min-h-[35rem] space-y-8 md:space-y-0 md:space-x-8 p-8 mt-20">
+    <section className="flex flex-col md:flex-row justify-between w-full min-h-[20rem] space-y-4 md:space-y-0 md:space-x-8 p-8 mt-20">
+      <div className="flex-1 md:flex-[3/4] space-y-4">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white">
+          Yash Thakur
+        </h1>
 
-        <div className="flex-1 md:flex-[1/3] text-left p-4 rounded-full shadow-md md:mr-8">
-          <div className="w-50 h-50 sm:w-60 sm:h-60 md:w-80 md:h-80 lg:w-96 lg:h-96 mx-auto rounded-full overflow-hidden md:mx-0">
-            <Image
-              src="/assets/images/profile-photo.jpeg"
-              alt="Yash Thakur"
-              width={400}
-              height={400}
-              className="rounded-full object-cover"
-            />
-          </div>
-        </div>
+        <p className="text-xl text-neutral-400">
+          Building{" "}
+          <span className="text-white font-semibold">
+            cool things
+          </span>
+        </p>
 
-        <div className="flex-1 md:flex-[2/3] p-4 rounded-lg shadow-md">
-          <div className="relative z-10 text-4xl sm:text-6xl md:text-7xl  
-          bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 font-sans font-bold">
-            Yash Thakur
-          </div>
-
-          <h4 className="relative z-10 text-3xl sm:text-5xl md:text-4xl  
-          bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-200">
-            Full Stack Developer
-          </h4>
-          <h4 className="relative z-10 text-3xl sm:text-5xl md:text-4xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-500">
-            ~ From concept to code, making it happen.
-          </h4>
-        </div>
-      </section>
-    </motion.div>
+        <p className="text-md text-neutral-400">
+          Full Stack Developer building products and web apps. Find me
+          on{" "}
+          <Link href="https://twitter.com/sincerelyyyash" className="text-white font-semibold hover:underline"
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            twitter
+          </Link>{" "}
+          for updates.
+        </p>
+      </div>
+      <div className="w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48 mx-auto rounded-md overflow-hidden md:mx-0">
+        <Image
+          src="/assets/images/profile-photo.jpeg"
+          alt="Profile Image"
+          width={100}
+          height={100}
+          className="rounded-md object-cover"
+        />
+      </div>
+    </section>
   );
 }
