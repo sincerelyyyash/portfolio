0abd79e81c9e55dde6d954c94eeca84eae7beb36
sincerelyyyash <yta0088@gmail.com>
2025-02-08 00:00:55 +0530

    Publication Section added

---
+++ components/PublicationSection.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from 'react';
+
+export function PublicationSection() {
+  return (
+    <div id='work'>
+      <h4 className="pt-10 relative z-10 text-lg md:text-4xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-600 to-neutral-200 mt-4 mb-2">
+        Publications
+      </h4>
+
+      <div className="flex flex-wrap justify-start gap-4">
+      </div>
+    </div>
+  );
+}
+
+
