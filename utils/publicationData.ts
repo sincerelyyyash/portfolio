eb80ba0ecf5a753b7dae07cbbd1d6874ba18cd2f
Yash Thakur <62953198+sincerelyyyash@users.noreply.github.com>
2025-06-03 23:46:53 +0530

    Update publicationData.ts

--- utils/publicationData.ts
+++ utils/publicationData.ts
@@ -14,5 +14,11 @@ export const publications: Publication[] = [
     "date": "June, 2023",
     "description": "Developed a high-security encryption framework combining AES, RSA, and Steganography after extensive research on encryption techniques. Built a Python program utilizing AES and RSA. The paper includes encryption methods, literature review, and the proposed framework.",
     "link": "https://www.iosrjournals.org/iosr-jce/papers/Vol25-issue3/Ser-2/E2503023340.pdf",
+  },{
+    "title": "Optimizing Stock Portfolio Allocation with Deep Reinforcement Learning",
+    "journal": "Kronika Journal",
+    "date": "May, 2025",
+    "description": "Developed a DRL-based stock portfolio optimization framework using PPO, A2C, and SAC, focusing on risk-adjusted returns. Built a custom OpenAI Gym environment with transaction costs and technical indicators. PPO showed strong performance in managing risk and drawdowns.",
+    "link": "https://kronika.ac/wp-content/uploads/19-KKJ2564.pdf",
   }
 ]
